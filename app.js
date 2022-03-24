0ad0a4359eb3712e57a81df1bb15a0f96ce88e92
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-03-24 17:28:44 +0900

    2.1

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
 const canvas= document.getElementById("jsCanvas");
+const ctx= canvas.getContext("2d");
+
+ctx.strokeStyle= "#2c2c2c";
+ctx.lineWidth= 2.5;
 
 let painting= false;
-
-function onMouseMove(event){
-    const x= event.offsetX;
-    const y= event.offsetY;
-}
-
-function onmousedown(event){
-    painting= true;
-}
 
 function onMouseUp(event){
     stopPainting();
 }
+
+function onMouseDown(event){
+    painting= true;
+}
+
+function onMouseMove(event){
+    const x= event.offsetX;
+    const y= event.offsetY;
+
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    } else {
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
+}
+
+function startPainting(event){
+    painting= true;
+}
+
 function stopPainting(event){
     painting= false;
 }
 
+
+
 if (canvas){
-    canvas.addEventListener("mousemove", onMouseMove); 
-    canvas.addEventListener("mousedown", onmousedown); 
-    canvas.addEventListener("mouseup", onMouseUp); 
+    canvas.addEventListener("mousemove", onMouseMove0)
+    canvas.addEventListener("mousedown", startPainting); 
+    canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("mouseup", stopPainting); 
 }
